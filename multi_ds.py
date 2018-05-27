10fbb37a3782dc3d1f5aedbe129f15bbf2835995
Vivin George <vivinhere@gmail.com>
2018-05-27 17:21:29 +0530

    Datastructure to do add, delete, find and findrandom operations with O(1) complexity

---
+++ multi_ds.py
@@ -0,0 +1,69 @@
+import random
+
+
+class Ds:
+
+    def __init__(self):
+        self.hash = {}
+        self.dict = {}
+
+    def insert(self, value):
+
+        if value not in self.hash:
+            n = len(self.dict)
+            self.dict[n] = value
+            self.hash[value] = n
+            print("Inserted")
+
+    def delete(self, value):
+
+        if value in self.hash:
+            n = len(self.dict)
+            last_value = self.dict[n-1]
+            value_position = self.hash[value]
+            self.dict[self.hash[value]] = last_value
+            del self.dict[n-1]
+            del self.hash[value]
+            self.hash[last_value] = value_position
+            print("Deleted")
+
+    def find(self, value):
+
+        if value in self.hash:
+            print(value, " is found at position ", self.hash[value], " at 2nd associative array")
+        else:
+            print("Not found")
+
+    def random(self):
+        n = len(self.dict)
+        if n > 0:
+            k = random.randint(0, n-1)
+            print(self.dict[k])
+
+    def show_all(self):
+        print(self.hash)
+        print(self.dict)
+
+
+
+ds = Ds()
+ds.insert("banana")
+ds.insert("orange")
+ds.insert("mango")
+ds.insert("rambuttan")
+ds.insert("apple")
+ds.show_all()
+ds.find("mango")
+ds.random()
+ds.random()
+ds.random()
+ds.random()
+ds.random()
+ds.random()
+ds.random()
+ds.show_all()
+ds.delete("mango")
+ds.show_all()
+ds.insert("mango")
+ds.show_all()
+print("==============================")
